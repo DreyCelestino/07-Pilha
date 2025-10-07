ede16ce9554c8b884a844c0013f033881ac0e9e7
DreyCelestino <andreycelestino05@gmail.com>
2025-10-07 12:00:57 -0300

    feito ez

--- Pilha/Pilha/Pilha.cpp
+++ Pilha/Pilha/Pilha.cpp
@@ -14,6 +14,7 @@ void menu();
 void inicializar();
 void pop();
 void push();
+void exibir();
 //--------------------------
 
 
@@ -25,14 +26,15 @@ int main()
 void menu()
 {
 	int op = 0;
-	while (op != 4) {
+	while (op != 5) {
 		system("cls"); // somente no windows
 		cout << "Menu Pilha";
 		cout << endl << endl;
 		cout << "1 - Inicializar Pilha \n";
 		cout << "2 - Inserir elemento (Push) \n";
 		cout << "3 - Remover elementos (Pop) \n";
-		cout << "4 - Sair \n";
+		cout << "4 - Exibir elementos" << endl;
+		cout << "5 - Sair \n";
 
 
 		cout << "Opcao: ";
@@ -46,7 +48,9 @@ void menu()
 			break;
 		case 3: pop();
 			break;
-		case 4:
+		case 4: exibir();
+			break;
+		case 5:
 			return;
 		default:
 			break;
@@ -71,6 +75,8 @@ void inicializar()
 	topo = NULL;
 	cout << "Pilha inicializada \n";
 
+
+	//aqui ja estava funcionando, entao deixei quieto.
 }
 
 
@@ -88,12 +94,49 @@ void push()
 	novo->prox = NULL;
 
 
+	NO* aux = topo;
+
+	topo = novo;
+	topo->prox = aux;
+	
+	cout << "Elemento " << topo->valor << " inserido na pilha." << endl;
+	
 }
 
 void pop()
 {
+	NO* aux = topo;
+	if (aux == NULL) {
+		cout << "Lista vazia." << endl;
+		return;
+	}
 
-	
+	cout << aux->valor << " removido." << endl;
+	topo = topo->prox;
+	free(aux);
 
 }
 
+void exibir()
+{
+	NO* aux = topo;
+	if (aux == NULL) {
+		cout << "Lista vazia." << endl;
+		return;
+	}
+
+	cout << "Conteudo da pilha" << endl;
+
+
+	//esse primeiro é puramente pra ficar bonito, 
+	//é possivel fazer so pelo while.
+	cout << aux->valor;
+	aux = aux->prox;
+
+	while (aux != NULL) {
+		cout << " -> " << aux->valor;
+		aux = aux->prox;
+	
+	}
+	cout << endl;
+}
